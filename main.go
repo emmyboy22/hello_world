69542e5c9f1c2e06cc7bd56f3b237059c621f285
unknown <mekabenson22@gmail.com>
2023-06-08 10:24:31 +0100

    football_club

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var car string 
+	var clothe string
+	
+	car = "camery"
+	name = "emeka"
+	clothe = "agbada"
+
+	var age int
+	var numKid int
+	var shoes int
+
+	age = 45
+	numKid = 9
+	shoes = 4
+
+	fmt.Fprintf("")
+
+}
+
+// import "fmt"
+
+// func main() {
+// 	car := "volvo"
+// 	name := "chidi"
+// 	cloth := "agbada"
+
+// 	var age int
+// 	var numKids int
+// 	var shoes int
+
+// 	age = 46
+// 	numKids = 2
+// 	shoes = 3
+
+// 	fmt.Printf("The guy's name is %s and the name of his car is %s and he also likes wearing %s. He is %dyrs old, has %d kids and also has %d of pairs shoes", name, car, cloth, age, numKids, shoes)
+
+// }
+
+
+
