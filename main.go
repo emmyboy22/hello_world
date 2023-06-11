0fa4a8c17e1be5bf89e011d51d69941f74dea670
unknown <mekabenson22@gmail.com>
2023-06-11 09:27:18 +0100

    function assingment completed

--- main.go
+++ main.go
@@ -2,47 +2,66 @@ package main
 
 import "fmt"
 
-// func addition(num4, num5 int) int {
-// 	answer := num4 + num5
-// 	return answer
-// }
+// // func addition(num4, num5 int) int {
+// // 	answer := num4 + num5
+// // 	return answer
 
-// func substraction(num1, num2 int) int  {
-// 	answer := num1 - num2
-// 	return answer
 
-// type DivisionResult struct {
-// 	Quotient int
-// 	Remainder int
-// }
+// // func substraction(num1, num2 int) int  {
+// // 	answer := num1 - num2
+// // 	return answer
 
-// func divide(divide, divisor int) DivisionResult  {
-// 		Quotient := 180 / 60
-// 		Remainder:= 60 % 20
+// // type DivisionResult struct {
+// // 	Quotient int
+// // 	Remainder int
+// // }
 
-// 		result := DivisionResult{
-// 			Quotient: Quotient,
-// 			Remainder: Remainder,
-// 		}
+// // func divide(divide, divisor int) DivisionResult  {
+// // 		Quotient := 180 / 60
+// // 		Remainder:= 60 % 20
+
+// // 		result := DivisionResult{
+// // 			Quotient: Quotient,
+// // 			Remainder: Remainder,
+// // 		}
 		
-// 		return result
+// // 		return result
 
-func multiply()  {
+// // func multiplication(a, b int) int  {
+
+// //      return a * b
+
+// // func additionAndSubtraction(num1, num2 int) (int, int) {
+// // 	answer := num1 + num2
+// // 	answer2 := num1 - num2
+// // 	return answer, answer2
+// // }
+
+
 	
-}
-// 	}
+// // func main()  {
+// 	// answer := addition(40, 60)
+// 	// fmt.Println(answer)
 
+// 	// answer := substraction(60, 45)
+// 	// fmt.Println(answer)
 
-func main()  {
-	// answer := addition(40, 60)
-	// fmt.Println(answer)
+// 	// dividend := 13 
+// 	// divisor := 5
 
-	// answer := substraction(60, 45)
-	// fmt.Println(answer)
+// 	// result := divide(dividend, divisor)
+// 	// fmt.Println("Quotient: %d, Remainder: %d\n", result.Quotient,result.Remainder)
 
-	// dividend := 13 
-	// divisor := 5
+// 	// var num1, num2 int
+// 	// fmt.Print("100")
+// 	// fmt.Scanln(&num1)
+// 	// fmt.Print("10")
+// 	// fmt.Scanln(&num2)
+// 	// result := multiplication(10,100)
+// 	// fmt.Println("Multiplicatio Result:", result)
 
-	// result := divide(dividend, divisor)
-	// fmt.Println("Quotient: %d, Remainder: %d\n", result.Quotient,result.Remainder)
-}
+//     // showAdd, showSubtraction := additionAndSubtraction (70,50)
+// 	// fmt.Println(showAdd, showSubtraction)
+
+	
+// }
