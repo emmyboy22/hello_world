fadabe05a9b056b48d3a800f8ac8fa67690ff63a
unknown <mekabenson22@gmail.com>
2023-06-14 10:49:46 +0100

    pointer

--- godwin.go
+++ godwin.go
@@ -11,6 +11,7 @@ func main2() {
 	name = "emeka"
 	clothe = "agbada"
 
+
 	var age int
 	var numKid int
 	var shoes int
@@ -20,5 +21,38 @@ func main2() {
 	shoes = 4
 
 	fmt.Fprintf("")
+
+
+
+	func addition(num4, num5 int) int {
+		answer := num4 + num5
+		return answer
+	}
+	
+	func substraction(num1, num2 int) int  {
+		answer := num1 - num2
+		return answer
+	}
+	
+	func divide(num1, num2 int) int  {
+		answer := num1 / num2
+		return answer
+	}
+	
+	func main()  {
+		answer := addition(60, 60)
+		fmt.Println(answer)
+	
+		anything := substraction(100,50)
+		fmt.Println(anything)
+	
+		anything = divide(200, 100)
+		fmt.Println(anything)
+	
+	
+	}
+	
+	
+	
 
 }
